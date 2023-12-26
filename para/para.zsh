d1228ec5ce298d3e846ab78186060c059c91a35a
paradox8599 <paradox8599@outlook.com>
2023-12-26 17:09:49 +1100

    scripts

---
+++ para/para.zsh
@@ -0,0 +1,12 @@
+export GHURL="git@github.com:paradox8599"
+
+alias vim="nvim"
+alias vi="nvim"
+
+alias ta="tmux -u attach -t default"
+alias tn="tmux -u new -s default"
+alias t="ta || tn"
+alias lg="lazygit"
+
+alias sf="screenfetch 2>/dev/zero"
+
