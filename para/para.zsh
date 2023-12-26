6c193fa7d8aeed9c6ee58ffb3b9d44994a11ebcb
paradox8599 <paradox8599@outlook.com>
2023-12-26 21:38:31 +1100

    tmux aliases

--- para/para.zsh
+++ para/para.zsh
@@ -1,12 +1,13 @@
 export GHURL="git@github.com:paradox8599"
 
-alias vim="nvim"
 alias vi="nvim"
+alias vim="nvim"
 
-alias ta="tmux -u attach -t default"
-alias tn="tmux -u new -s default"
-alias t="ta || tn"
+alias tad="tmux -u attach -t default"
+alias tnd="tmux -u new -s default"
+alias t="tad || tnd"
+alias ta="tmux -u attach -t"
+alias tn="tmux -u new -s"
+
 alias lg="lazygit"
-
-alias sf="screenfetch 2>/dev/zero"
 
