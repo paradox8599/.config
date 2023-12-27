73f13d8ad4abff3697a382af51d49b513f92c065
paradox8599 <paradox8599@outlook.com>
2023-12-28 07:16:00 +1100

    fix: tmux alias not correct

--- para/para.zsh
+++ para/para.zsh
@@ -7,9 +7,11 @@ alias lg="lazygit"
 
 alias tad="tmux -u attach -t default"
 alias tnd="tmux -u new -s default"
-alias t="tad || tnd"
+# alias t="tad || tnd"
 alias ta="tmux -u attach -t"
 alias tn="tmux -u new -s"
+alias t='[[ $(tmux ls 2>/dev/null | rg -v attached | wc -l) -gt 0 ]] && tmux attach -t $(tmux ls | rg -v attach | cut -d":" -f1 | tr "\n" " " | cut -d" " -f1) || tmux'
+alias tl='tmux ls 2>/dev/null'
 
 # start hyprland
 if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
@@ -18,10 +20,11 @@ fi
 
 # start tmux
 if command -v tmux &> /dev/null \
-    && [ -n "$PS1" ] \
-    && [[ ! "$TERM" =~ screen ]] \
-    && [[ ! "$TERM" =~ tmux ]] \
-    && [ -z "$TMUX" ]; then
+        && [ -n "$PS1" ] \
+        && [[ ! "$TERM" =~ screen ]] \
+        && [[ ! "$TERM" =~ tmux ]] \
+        && [ -z "$TMUX" ] \
+        && [ "$XDG_VTNR" = 1 ]; then
     # attach to detached session if there is any
     # otherwise create a new session
     detached_count=$(tmux ls 2>/dev/null | rg -v attached | wc -l)
